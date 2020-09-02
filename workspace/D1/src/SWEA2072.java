86d312a23b8514752d812f1ff48770edb8187db8
BonHyuck <qhsgur0126@gmail.com>
2020-09-02 21:04:16 +0900

    살짝 수정

--- workspace/D1/src/SWEA2072.java
+++ workspace/D1/src/SWEA2072.java
@@ -1,13 +1,6 @@
 // 10개의 수 중에 홀수만 더하기
 import java.util.Scanner;
 public class SWEA2072 {
-	/*
-	 * 
-		3
-		3 17 1 39 8 41 2 32 99 2
-		22 8 5 123 7 2 63 7 3 46
-		6 63 2 3 58 76 21 33 8 1 
-	 * */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T=sc.nextInt();
